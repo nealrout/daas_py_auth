25f3625d712d7a66a22b71c38019e49da2f05b11
nealrout <nroutson@gmail.com>
2025-02-17 14:44:18 -0500

    cleanup

--- daas_py_auth/daas_py_auth/settings.py
+++ daas_py_auth/daas_py_auth/settings.py
@@ -26,7 +26,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-*o#a#9kg+)clbvxigz!z^gg$1ga9fj3$aj!h76u-!&$gazgdds'
 SECRET_KEY = config.get_secret('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
